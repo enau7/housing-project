202182466cca1e21f03008f9956fcf15462745dd
enau7 <77908185+enau7@users.noreply.github.com>
2022-04-10 07:56:44 -0700

    Add files via upload

--- scrapetest.py
+++ scrapetest.py
@@ -1,11 +1,15 @@
 import scraper as s
+import time
 
 def usd_to_int(arr):
     for i in range(0,len(arr)):
+        if arr[i] == 'null':
+            arr[i] = None
+            continue
         arr[i] = arr[i].replace("$","")
         arr[i] = arr[i].replace(",","")
         arr[i] = arr[i].replace(" ","")
-        arr[i] = int(arr[i])
+        arr[i] = float(arr[i])
 
 def link_process(arr):
     for i in range(0,len(arr)):
@@ -21,19 +25,21 @@ url = "https://www.zillow.com/browse/homes/ca/santa-barbara-county/93117/27/"
 sindx1 = '"/homedetails/'
 sindx2 = '"'
 banned = ''
-    
-links = s.inscraper(url,sindx1,sindx2,banned).scrape()
+links = s.scraper(url,sindx1,sindx2,banned).scrape()
 link_process(links)
 print(links)
 
-mindx1 = '<span class="hdp__sc-reo5z7-1 hvBPYp">"'
-mindx2 = '"'
+mindx1 = r'\"taxPaid\":'
+mindx2 = ','
+banned = ''
 
 homes = []
 
-#for k in range(0,len(links)):
-#    history = s.inscraper(links[k],mindx1,mindx2,banned).scrape()
-#    usd_to_int(history)
-#    homes.append(history)
-#
-#print(homes)
+for k in range(0,len(links)):
+    time.sleep(2)
+    history = s.scraper(links[k],mindx1,mindx2,banned).scrape()
+    usd_to_int(history)
+    print(history)
+    homes.append(history)
+
+print(homes)
